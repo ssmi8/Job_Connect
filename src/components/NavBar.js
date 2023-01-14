3a6c784c44dfabc1658a369cc42c33b69bf3a8ab
ssmi8 <smithsteve8@hotmail.com>
2023-01-14 17:53:22 +0000

    Adjusted navbar for logged in user - added feed, liked, skills, jobs, signout and profile links in NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,66 @@ import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const addPostIcon = (
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/posts/create"
+    >
+      <i className="far fa-plus-square"></i>Add post
+    </NavLink>
+  )
+
+  const loggedInIcons = (
+  <>
+   <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/feed"
+    >
+      <i className="fas fa-stream"></i>Feed
+    </NavLink>
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/liked"
+    >
+      <i className="fas fa-heart"></i>Liked
+    </NavLink>
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/skills"
+    >
+      <i className="fas fa-file"></i>Skills
+    </NavLink>
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/jobs"
+    >
+      <i className="fas fa-briefcase"></i>Jobs
+    </NavLink>
+    <NavLink
+    className={styles.NavLink}
+    to="/"
+    onClick={() => {}}
+    >
+      <i className="fas fa-sign-out-alt"></i>Signout
+    </NavLink>
+    <NavLink
+    className={styles.NavLink}
+    to={`/profiles/${currentUser?.profile_id}`}
+    >
+      <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+    </NavLink>
+  </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -36,7 +91,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
