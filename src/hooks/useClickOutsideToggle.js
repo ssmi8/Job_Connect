b52908d0ffd67abc0aab9fb20520824d1cf6f57e
ssmi8 <smithsteve8@hotmail.com>
2023-01-15 11:41:45 +0000

    created ref & eventhandler for burger menu and created custom hook to handle closing navbar menu

---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef ,useState } from "react";
+
+const useClickOutsideToggle = () => {
+
+    // custom hook for burger menu so that menu toggle can be clicked anywhere on screen
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)){
+        setExpanded(false)
+      }
+    }
+    document.addEventListener('mouseup', handleClickOutside)
+    return () => {
+      document.removeEventListener('mouseup', handleClickOutside)
+    } 
+  }, [ref]);
+
+  return { expanded, setExpanded, ref };
+    
+  
+}
+
+export default useClickOutsideToggle
