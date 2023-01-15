3b9988295d7f5a37b799c854b06f8f26c771f6f7
ssmi8 <smithsteve8@hotmail.com>
2023-01-15 16:29:35 +0000

    update post to show users post, profile, updated, content title, image and images for likes and comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from 'react-router';
-import { axiosReq } from '../../api/axiosDefaults';
-
-
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
-    const {id} = useParams();
+    const { id } = useParams();
     // makes all future logic compatible with arrays of posts
-    const [posts, setPost] = useState({results: [] });
+    const [post, setPost] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
-            try {
-                //destructuring and renaming the variable in place
-                const [{data: post}] = await Promise.all([
-                    axiosReq.get(`/posts/${id}`)
-                ])
-                setPost({results: [post]})
-                console.log(post)
-            } catch(err){
-                console.log(err)
-            }
-        };
-        handleMount();
-    }, [id]);
+          try {
+                // destructuring and renaming the variable in place
+                const [{ data: post }] = await Promise.all([
+                    axiosReq.get(`/posts/${id}`),
+                  ]);
+                  setPost({ results: [post] });
+                  console.log(post);
+                } catch (err) {
+                  console.log(err);
+                }
+              };
+              handleMount();
+  }, [id]);
+
   return (
-    <Row className='h-100'>
-        <Col className='py-2 p-0 p-lg-2' lg={8}>
-            <p>Popular profiles for mobile</p>
-            <p>Post Component</p>
-            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-                Popular profiles for desktop
-            </Col>
-        </Col>
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+            {/* spread the post object from the results array so key value pairs are passed in as props */}
+            <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>Comments</Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
     </Row>
   );
-}
+} 
 
 export default PostPage
