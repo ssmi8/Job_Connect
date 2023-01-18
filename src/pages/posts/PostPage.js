89627db733e7bbf17ecfaa2ba6de4cbb5447789b
ssmi8 <smithsteve8@hotmail.com>
2023-01-18 20:31:19 +0000

    Create CommentCreateForm and styling, with comment functionality

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,20 +9,29 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
+
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostPage() {
     const { id } = useParams();
     // makes all future logic compatible with arrays of posts
     const [post, setPost] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
     useEffect(() => {
         const handleMount = async () => {
           try {
                 // destructuring and renaming the variable in place
-                const [{ data: post }] = await Promise.all([
+                const [{ data: post }, { data: comments }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`)
                   ]);
                   setPost({ results: [post] });
-                  console.log(post);
+                  setComments(comments);
                 } catch (err) {
                   console.log(err);
                 }
@@ -36,7 +45,30 @@ function PostPage() {
         <p>Popular profiles for mobile</p>
             {/* spread the post object from the results array so key value pairs are passed in as props */}
             <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+        {currentUser ? (
+          <CommentCreateForm
+          profile_id={currentUser.profile_id}
+          profileImage={profile_image}
+          post={id}
+          setPost={setPost}
+          setComments={setComments}
+        />
+      ) : comments.results.length ? (
+        "Comments"
+      ) : null}
+      {comments.results.length ? (
+        comments.results.map((comment) => (
+          <p key={comment.id}>
+            {comment.owner}: {comment.content}
+          </p> 
+        ))
+      ) : currentUser ? (
+        <span>No comments yet, be the first to comment!</span>
+      ) : (
+        <span>No comments... yet</span>
+      )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
