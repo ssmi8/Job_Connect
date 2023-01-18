f98e38edb1808ed7c6283d1dce4e33330df10c6e
ssmi8 <smithsteve8@hotmail.com>
2023-01-18 20:48:56 +0000

    Creat Comment component to display users comments with profile name, avatar and how long ago commented

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,6 +8,7 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment"
 
 
 import CommentCreateForm from "../comments/CommentCreateForm";
@@ -59,9 +60,7 @@ function PostPage() {
       ) : null}
       {comments.results.length ? (
         comments.results.map((comment) => (
-          <p key={comment.id}>
-            {comment.owner}: {comment.content}
-          </p> 
+          <Comment key={comment.id} {...comment} />
         ))
       ) : currentUser ? (
         <span>No comments yet, be the first to comment!</span>
