c5c0fdc319064017a1fe5ba9e0c293a69b7ccffb
ssmi8 <smithsteve8@hotmail.com>
2023-01-20 20:55:12 +0000

    update Context.Provider for our profileData so that it is more accessible throughout the app

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,43 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
 
-const PopularProfiles = ({ mobile } ) => {
-  const [profileData, setProfileData] = useState({
-    // we will use the pageProfile later!
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-  const { popularProfiles } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          popularProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+const PopularProfiles = ({ mobile }) => {
+  const { popularProfiles } = useProfileData();
 
   return (
-    <Container 
-        className={`${appStyles.Content} 
-            ${mobile && 'd-lg-none text-center mb-3'}`
-        }>
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
       {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React from "react";
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Link } from 'react-router-dom';
-import Avatar from '../../components/Avatar';
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
-    const {profile, mobile, imageSize=55} = props;
-    const {id, following_id, image, owner} = profile;
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
 
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
-    <div 
-        className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
     >
-        <div>
-            <Link className='align-self-center' tp={`/profiles/${id}`}>
-                <Avatar src={image} height={imageSize} />
-            </Link>
-        </div>
-        <div className={`mx-2 ${styles.WordBreak}`}>
-            <strong>{owner}</strong>
-        </div>
-        <div className={`text-right ${!mobile && "ml-auto"}`}>
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
         {!mobile &&
-          currentUser && !is_owner &&
+          currentUser &&
+          !is_owner &&
           (following_id ? (
             <Button
               className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
@@ -43,9 +44,9 @@ const Profile = (props) => {
               follow
             </Button>
           ))}
-        </div>
+      </div>
     </div>
-  )
+  );
 };
 
-export default Profile
+export default Profile;
