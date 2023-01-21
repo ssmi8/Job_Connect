b19609468740e047cab2d7680cb4792e254ed77d
ssmi8 <smithsteve8@hotmail.com>
2023-01-21 16:17:42 +0000

    add userRedirtects so users are redirected so that can't access pages they shouldn't be able to access

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -16,9 +16,11 @@ import appStyles from "../../App.module.css";
 import sign_in from "../../assets/sign_in.jpg";
 import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() {
     const setCurrentUser = useSetCurrentUser();
+    useRedirect('loggedIn')
 
     const [signInData, setSignInData] = useState({
         username: "",
@@ -35,7 +37,7 @@ function SignInForm() {
         try {
             const {data} = await axios.post("/dj-rest-auth/login/", signInData);
             setCurrentUser(data.user)
-            history.push("/");
+            history.goBack();
         } catch (err) {
           setErrors(err.response?.data);  
         }
