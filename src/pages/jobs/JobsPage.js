1b44f8687d10441a975a450858fa89ece586c466
ssmi8 <smithsteve8@hotmail.com>
2023-01-25 13:04:38 +0000

    updated text search functionality for job postings

--- src/pages/jobs/JobsPage.js
+++ src/pages/jobs/JobsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,14 +7,85 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/JobsPage.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Job from "./Job";
 
-function JobsPage() {
+import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utlis";
+import Asset from "../../components/Asset";
+import PopularProfiles from "../profiles/PopularProfiles";
+
+function JobsPage({message, filter = ""}) {
+    const [jobs, setJobs] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    const [query, setQuery]= useState("");
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const { data } = await axiosReq.get(`/jobs/?${filter} search=${query}`)
+                setJobs(data)
+                setHasLoaded(true)
+            } catch(err){
+                console.log(err)
+    
+            }
+        };
+        setHasLoaded(false);
+    /* to stop the search requests after every key typed - delay response */
+    const timer = setTimeout(() => {
+      fetchJobs();
+    }, 1000)
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [filter, query ,pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+      <PopularProfiles mobile />
+        {/* search bar and query to search profiles/posts */}
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form className={styles.SearchBar}
+        onSubmit={(event) => event.preventDefault()}
+        >
+         <Form.Control
+         value={query}
+         onChange={(event) => setQuery(event.target.value)}
+         type="text"
+         className="mr-sm-2"
+         placeholder="Search jobs"
+         />            
+        </Form>
+
+        {hasLoaded ? (
+            <>
+            {jobs.results.length ? (
+              <InfiniteScroll
+                children={
+                    jobs.results.map((job) => (
+                        <Job key={job.id} {...job} setJobs={setJobs} />
+                      ))
+                }
+                dataLength={jobs.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!jobs.next}
+                next={() => fetchMoreData(jobs, setJobs)}
+              />              
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+            console.log(('show loading spinner'))
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
