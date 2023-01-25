aca85e4a62a06ceb5b8e20806a9fc3ed7379c749
ssmi8 <smithsteve8@hotmail.com>
2023-01-25 19:30:53 +0000

    files created for skills section of app

---
+++ src/pages/skills/Skill.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Skill = (props) => {
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        comments_count,
+        likes_count,
+        like_id,
+        title,
+        content,
+        image,
+        updated_at,
+      } = props;
+
+      
+  return (
+    <div>Skill</div>
+  )
+}
+
+export default Skill
