bfe9cee46e0de796cca169910bef533aeb93c28c
ssmi8 <smithsteve8@hotmail.com>
2023-01-26 19:38:00 +0000

    Update SkillPage file to show posted Skills for users to view

---
+++ src/pages/skills/SkillPage.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+
+import Skill from './Skill';
+import PopularProfiles from '../profiles/PopularProfiles';
+
+function SkillPage() {
+    const { id } = useParams();
+
+    const [skill, setSkill] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: skill }] = await Promise.all([
+                    axiosReq.get(`/skills/${id}`),
+                ]);
+                setSkill({ results: [skill] });
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        handleMount();
+    }, [id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <Skill {...skill.results[0]} setSkill={setSkill} skillPage />
+      </Col>
+
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
+};
+
+export default SkillPage
