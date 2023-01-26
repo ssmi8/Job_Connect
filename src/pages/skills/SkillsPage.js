98a7c1b35dc80f847b8d593da43037b783fde502
ssmi8 <smithsteve8@hotmail.com>
2023-01-26 20:06:32 +0000

    Update SkillsPage file to include search bar and user to see skills posted

---
+++ src/pages/skills/SkillsPage.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import { Col, Form } from 'react-bootstrap';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useLocation } from 'react-router-dom';
+import { axiosReq } from '../../api/axiosDefaults';
+import PopularProfiles from '../profiles/PopularProfiles';
+import Skill from '../../pages/skills/Skill';
+import Asset from '../../components/Asset';
+import { fetchMoreData } from '../../utils/utlis';
+
+function SkillsPage({ message, filter = ""}) {
+    const [skills, setSkills] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    const [query, setQuery] = useState("");
+
+    useEffect (() => {
+        const fetchSkills = async () => {
+            try {
+                const { data } = await axiosReq.get(`/skills/?${filter}search${query}`);
+                setSkills(data);
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        setHasLoaded(false);
+        const timer = setTimeout(() => {
+            fetchSkills();
+        }, 1000)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [filter, query, pathname]);
+
+
+  return (
+    <Row className="h-100">
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <PopularProfiles mobile />
+            <i className={`fas fa-search ${styles.SearchIcon}`} />
+            <Form className={styles.SearchBar}
+            onSubmit={(event) => event.preventDefault()}
+            >
+                <Form.Control
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+                type="text"
+                className="mr-sm-2"
+                placeholder="Search to learn new skills!"
+                />
+            </Form>
+
+            {hasLoaded ? (
+                <>
+                    {skills.results.length ? (
+                        <InfiniteScroll
+                        children={
+                            skills.results.map((skill) => (
+                                <Skill key={skill.id} {...skill} setSkills={setSkills} />
+                            ))
+                        }
+                        dataLength={skills.results.length}
+                        loader={<Asset spinner />}
+                        hasMore={!!skills.next}
+                        next={() => fetchMoreData(skills, setSkills)} 
+                        />
+                    ) : (
+                        <Container className={appStyles.Content}>
+                            <Asset src={NoResults} message={message} />
+                        </Container>
+                    )}
+                </>
+            ) : (
+                <Container className={appStyles.Content}>
+                  <Asset spinner />
+                </Container>
+              )}
+        </Col>
+        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+            <PopularProfiles />
+        </Col>
+    </Row>    
+  );
+}
+
+export default SkillsPage;
