e818bd774f612d53b0b4cee69dc27e8740b58ec4
ssmi8 <smithsteve8@hotmail.com>
2023-01-26 11:59:53 +0000

    update Skill and SKillCreateForm files to upload skill sections for main app

--- src/pages/skills/Skill.js
+++ src/pages/skills/Skill.js
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from "../../styles/Skill.module.css";
+import { useHistory } from 'react-router-dom';
+import { axiosRes } from '../../api/axiosDefaults';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Card } from 'react-bootstrap';
+import Avatar from '../../components/Avatar';
+import { MoreDropdown } from '../../components/MoreDropdown';
 
 const Skill = (props) => {
     const {
@@ -6,18 +13,55 @@ const Skill = (props) => {
         owner,
         profile_id,
         profile_image,
-        comments_count,
-        likes_count,
-        like_id,
-        title,
         content,
         image,
         updated_at,
       } = props;
 
+      const currentUser =useCurrentUser();
+      const is_owner = currentUser?.username === owner;
+      const history = useHistory();
+
+      const handleEdit = () => {
+        history.push(`/skills/${id}/edit`)
+      }
+
+      const handleDelete = async () => {
+        try {
+          await axiosRes.delete(`/skills/${id}`);
+          history.goBack();
+        } catch (err) {
+          console.log(err);
+        }
+      };
       
   return (
-    <div>Skill</div>
+    <Card className={styles.Skill}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && JobPage && (
+              <MoreDropdown
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+              />
+            )}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+      </Card.Body>
+    </Card>
   )
 }
 
